201760da35713a3401e8a60161498d45f681bcbc
Yuri Kirilin <yura.kirilin@gmail.com>
2018-02-22 23:01:22 +0300

    Task: love-triangle solution

--- src/index.js
+++ src/index.js
@@ -3,5 +3,27 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+	var num = 0;
+  	var trVertex = [];
+  	var lOne, lTwo, lThree 
+
+  	preferences.unshift(0);
+  	  	
+  	for (var i = 1; i < preferences.length; i++) {
+    	lOne = preferences[i];
+    	lTwo = preferences[lOne];
+    	lThree = preferences[lTwo];
+
+    	if (lOne !== lTwo && lOne !== lThree && lTwo !== lThree &&
+      		lThree === i) {
+      		if (trVertex.indexOf(i) === -1) {
+        		trVertex.push(lOne);
+        		trVertex.push(lTwo);
+        		trVertex.push(lThree);
+        		num ++;
+      		}
+    	}
+  	}
+  
+ 	return num;
 };
